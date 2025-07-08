b526364d3b55827f92aab720c9c6c05bc60423e7
jaindivyanshu1 <jaindivyanshu.personal@gmail.com>
2025-07-08 22:23:36 +0530

    Intial setup for Load Balancer

---
+++ src/test/java/com/example/dispatchLoadBalancer/DispatchLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dispatchLoadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
